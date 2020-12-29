502bc5787e62f1775d676f5477203435c925569c
Arphik <sebo44@gmail.com>
2020-12-29 15:12:37 +0100

    app.enableCors() ->  app.enableCors({ methods: UPDATE, POST, DELETE})

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  app.enableCors({ methods: 'UPDATE, POST, DELETE'});
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
