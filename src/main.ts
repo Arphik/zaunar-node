4cdda23e31d9c4e28ab4d8f28523b8eda95660de
Arphik <sebo44@gmail.com>
2020-12-29 15:42:03 +0100

    origin: true

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({ methods: 'UPDATE, POST, DELETE'});
+  app.enableCors({ methods: 'UPDATE, POST, DELETE, GET', origin: true});
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
